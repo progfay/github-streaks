94ac65a9aae363328b57c010542ea41b0255ee50
progfay <shun1856shun@gmail.com>
2020-04-12 23:13:18 +0900

    feat(lib/range): error handling for arguments

--- src/lib/range.ts
+++ src/lib/range.ts
@@ -1,4 +1,12 @@
 export const range = (start: number, end: number): number[] => {
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    throw new Error('First and second arguments of `range` function must be integer.')
+  }
+
+  if (start >= end) {
+    throw new Error('First argument of `range` function must be lower than second argument.')
+  }
+
   const r: number[] = []
   for (let i = start; i < end; i++) {
     r.push(i)
