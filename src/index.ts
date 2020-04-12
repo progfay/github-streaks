4a4091613ef358dbd996ae1c548d5e6945712a19
progfay <shun1856shun@gmail.com>
2020-04-12 17:11:11 +0900

    feat: typescriptify source code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-const fetch = require('node-fetch')
-const chalk = require('chalk')
-const { parse } = require('fast-html-parser')
+import fetch from 'node-fetch'
+import chalk from 'chalk'
+import { parse } from 'fast-html-parser'
+
+import type { HTMLElement } from 'fast-html-parser'
+
+type AttributesType = {
+  [key: string]: string
+}
 
 const GITHUB_USERNAME_REGEXP = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i
 
-const range = (start, end) => {
-  const r = []
+const range = (start: number, end: number): number[] => {
+  const r: number[] = []
   for (let i = start; i < end; i++) {
     r.push(i)
   }
   return r
 }
 
-const fetchElements = async (url, selector) => {
+const fetchElements = async (url: string, selector: string): Promise<HTMLElement[]> => {
   const response = await fetch(url)
   const html = await response.text()
   const root = parse(html, {
@@ -25,9 +31,9 @@ const fetchElements = async (url, selector) => {
   return elements
 }
 
-const getAttributes = elements => {
-  const attributes = {}
-  for (const attr of elements.rawAttrs.split(' ')) {
+const getAttributes = (element: HTMLElement) => {
+  const attributes: AttributesType = {}
+  for (const attr of element.rawAttrs.split(' ')) {
     const [key, ...value] = attr.split('=')
     attributes[key] = value.join('=').replace(/^"(.*)"$/, '$1')
   }
@@ -58,13 +64,13 @@ const main = async () => {
         fetchElements(`https://github.com/users/${username}/contributions?from=${year}-12-01&to=${year}-12-31`, 'rect.day')
           .then(days => days.map(day => getAttributes(day)))
           .then(days => days.map(day => ({ date: day['data-date'], count: parseInt(day['data-count'], 10) })))
-          .then(days => days.reduce((accumulator, day) => ({ ...accumulator, [day.date]: day.count }), {}))
+          .then(days => days.reduce((map, day) => map.set(day.date, day.count), new Map<string, number>()))
       ))
   )
 
-  const days = Object.entries(
-    years.reduce((accumulator, year) => ({ ...accumulator, ...year }), {})
-  ).sort((a, b) => a.date < b.date ? 1 : -1)
+  const days = Array.from((new Map<string, number>(
+    years.reduce((accumulator, year) => [...accumulator, ...year.entries()], [] as [string, number][]))).entries())
+  .sort((a, b) => a[0] < b[0] ? 1 : -1)
 
   const today = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`
   const todayIndex = days.findIndex(day => day[0] === today)
