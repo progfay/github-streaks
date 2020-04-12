b862d3315369023b8bc86da4a8ef7b0891621bad
progfay <shun1856shun@gmail.com>
2020-04-12 19:01:50 +0900

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.eslint.json'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
+  }
+}
