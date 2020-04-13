c2ea39fb2b5ee401f98819e9cb6e14c2c0f91ce0
progfay <shun1856shun@gmail.com>
2020-04-13 23:55:49 +0900

    refactor: remove comments

--- setupJest.ts
+++ setupJest.ts
@@ -4,10 +4,3 @@ const customGlobal = global as GlobalWithFetchMock
 customGlobal.fetch = require('jest-fetch-mock')
 customGlobal.fetch.enableMocks()
 customGlobal.fetchMock = customGlobal.fetch
-
-// import fetch from 'jest-fetch-mock'
-// const fetch = require('jest-fetch-mock')
-
-// global.fetch = fetch
-// global.fetchMock = fetch
-// jest.mock('node-fetch', () => (global.fetch))
