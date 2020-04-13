b064dbce50d73960649d3f322fef857c4bc5c67f
progfay <shun1856shun@gmail.com>
2020-04-13 23:48:55 +0900

    feat: error handling for arguments

--- src/lib/fetchElements.ts
+++ src/lib/fetchElements.ts
@@ -2,6 +2,14 @@ import fetch from 'node-fetch'
 import HTML from 'fast-html-parser'
 
 export const fetchElements = async (url: string, selector: string): Promise<HTML.HTMLElement[]> => {
+  if (!/^http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?$/.test(url)) {
+    throw new Error('First argument must be valid HTTP URL.')
+  }
+
+  if (!selector) {
+    throw new Error('Second argument must be not empty.')
+  }
+
   const response = await fetch(url)
   const html = await response.text()
   const root = HTML.parse(html, {
