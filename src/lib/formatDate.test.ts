725c4c65e751929ca79b2cfff0455310140befff
progfay <shun1856shun@gmail.com>
2020-04-15 23:13:09 +0900

    test: add test for formatDate

---
+++ src/lib/formatDate.test.ts
@@ -0,0 +1,13 @@
+
+import { formatDate } from './formatDate'
+
+describe('formatDate(date: Date) function', () => {
+  describe('takes a Date, `date` as argument', () => {
+  })
+
+  describe('return formatted date string like YYYY-MM-DD', () => {
+    it('formatDate(new Date(Date.UTC(2020, 7, 1))) return "2020-08-01"', () => {
+      expect(formatDate(new Date(Date.UTC(2020, 7, 1)))).toEqual('2020-08-01')
+    })
+  })
+})
