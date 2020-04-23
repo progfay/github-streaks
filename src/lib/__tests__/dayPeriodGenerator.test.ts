e0c039a0cc23a1369d6dcaf5094823b28beb1caf
progfay <shun1856shun@gmail.com>
2020-04-23 19:47:25 +0900

    test: replace Date with Day

--- src/lib/__tests__/dayPeriodGenerator.test.ts
+++ src/lib/__tests__/dayPeriodGenerator.test.ts
@@ -1,26 +1,22 @@
 import { dayPeriodGenerator } from './../dayPeriodGenerator'
+import { Day } from './../Day'
 
-describe('dayPeriodGenerator(start: Date, end: Date) generator function', () => {
-  describe('takes 2 Dates, `start` and `end` as arguments', () => {
-    // describe('`start` and `end` must be integer', () => {
-    //   it('dayPeriodGenerator(0.1, 5.5) throw error.', () => {
-    //     expect(() => dayPeriodGenerator(0.1, 5.5)).toThrow('First and second arguments of `dayPeriodGenerator` function must be integer.')
-    //   })
-    // })
+describe('dayPeriodGenerator(start: Day, end: Day) generator function', () => {
+  describe('takes 2 Day Objects, `start` and `end` as arguments', () => {
   })
 
   describe('return Generator object, iterate day from `start` to `end`', () => {
-    it('dayPeriodGenerator(Date{1 Apr 2020}, Date{1 Apr 2020}) return Generator[Date{1 Apr 2020}].', () => {
-      const generator = dayPeriodGenerator(new Date(2020, 3, 2), new Date(2020, 3, 2))
-      expect(generator.next().value).toEqual(new Date(2020, 3, 2))
+    it('dayPeriodGenerator(Day{1 Apr 2020}, Day{1 Apr 2020}) return Generator[Day{1 Apr 2020}].', () => {
+      const generator = dayPeriodGenerator(new Day(2020, 3, 2), new Day(2020, 3, 2))
+      expect(generator.next().value).toEqual(new Day(2020, 3, 2))
       expect(generator.next().done).toEqual(true)
     })
 
-    it('dayPeriodGenerator(Date{1 Apr 2020}, Date{3 Apr 2020}) return Generator[Date{1 Apr 2020}, Date{2 Apr 2020}, Date{3 Apr 2020}].', () => {
-      const generator = dayPeriodGenerator(new Date(2020, 3, 2), new Date(2020, 3, 4))
-      expect(generator.next().value).toEqual(new Date(2020, 3, 2))
-      expect(generator.next().value).toEqual(new Date(2020, 3, 3))
-      expect(generator.next().value).toEqual(new Date(2020, 3, 4))
+    it('dayPeriodGenerator(Day{1 Apr 2020}, Day{3 Apr 2020}) return Generator[Day{1 Apr 2020}, Day{2 Apr 2020}, Day{3 Apr 2020}].', () => {
+      const generator = dayPeriodGenerator(new Day(2020, 3, 2), new Day(2020, 3, 4))
+      expect(generator.next().value).toEqual(new Day(2020, 3, 2))
+      expect(generator.next().value).toEqual(new Day(2020, 3, 3))
+      expect(generator.next().value).toEqual(new Day(2020, 3, 4))
       expect(generator.next().done).toEqual(true)
     })
   })
