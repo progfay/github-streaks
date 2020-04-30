162c4def5c25005c7780609bdcdcfad9232d3d13
progfay <shun1856shun@gmail.com>
2020-04-30 22:59:57 +0900

    feat: highlight results

--- src/showStreak.ts
+++ src/showStreak.ts
@@ -1,9 +1,10 @@
+import chalk from 'chalk'
 import type { StreakStrategyType } from './type'
 
 export const showStreak = (contributions: Map<string, number>, streakStrategy: StreakStrategyType): void => {
   const { from, to, count } = streakStrategy(contributions)
   if (from && to && count > 0) {
-    console.log(`${from.toString()} ~ ${to.toString()} (${count} ${count === 1 ? 'days' : 'day'})`)
+    console.log(`${chalk.green(from.toString())} ~ ${chalk.green(to.toString())} (${chalk.green.bold(count)} ${count > 1 ? 'days' : 'day'})`)
   } else {
     console.log('No streak...')
   }
