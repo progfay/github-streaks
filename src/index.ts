960163fa9818cd60ba93dfb2df3d96f9a5d884ab
progfay <shun1856shun@gmail.com>
2020-10-03 11:47:16 +0900

    fix(All Contributions): trim all days only github account exists

--- src/index.ts
+++ src/index.ts
@@ -33,14 +33,21 @@ const main = async () => {
   const user = new GitHubUser(username)
   const { created_at: createdAt } = await user.getUserInfo()
   const joinedYear = parseInt(createdAt.substring(0, 4), 10)
-  const thisYear = new Date().getFullYear()
+  const joinedDay = createdAt.substring(0, 10)
+  const date = new Date()
+  const thisYear = date.getFullYear()
+  const today = `${thisYear}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
 
   const annualDailyContributionsMaps = await Promise.all(
     range(joinedYear, thisYear + 1)
       .map(year => user.getAnnualDailyContributions(year))
   )
 
-  const allDailyContributions = annualDailyContributionsMaps.flat()
+  const annualDailyContributions = annualDailyContributionsMaps.flat()
+  const allDailyContributions = annualDailyContributions.slice(
+    annualDailyContributions.findIndex(contribution => contribution.day === joinedDay),
+    annualDailyContributions.findIndex(contribution => contribution.day === today) + 1
+  )
   const currentStreak = getCurrentStreak(allDailyContributions)
   const longestStreak = getLongestStreak(allDailyContributions)
   const rows = [
