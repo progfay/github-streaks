bea2374caf012e512fbb7583f84ccd7d9c60819b
progfay <shun1856shun@gmail.com>
2020-11-18 23:45:05 +0900

    refactor(convertToTable): use Array.prototype.reduce for calc column length

--- src/lib/convertToTable.ts
+++ src/lib/convertToTable.ts
@@ -2,14 +2,10 @@ const bold = (str: string): string => `\u001b[1m${str}\u001b[22m`
 const dim = (str: string): string => `\u001b[2m${str}\u001b[22m`
 
 export function convertToTable<T extends { [key: string]: any }> (objects: T[], orderedKeys: (keyof T)[]) {
-  const columnLength = orderedKeys.map(key => key.toString().length)
-
-  for (let i = 0; i < orderedKeys.length; i++) {
-    const key = orderedKeys[i] as string
-    for (const object of objects) {
-      columnLength[i] = Math.max(columnLength[i], object[key].toString().length)
-    }
-  }
+  const columnLength = objects.reduce(
+    (lens, obj) => orderedKeys.map((key, i) => Math.max(lens[i], obj[key].toString().length)),
+    orderedKeys.map(key => key.toString().length)
+  )
 
   const rowDelimiter = dim('+' + columnLength.reduce((accr, len) => `${accr} ${'-'.repeat(len)} +`, ''))
   const cellDelimiter = dim('|')
