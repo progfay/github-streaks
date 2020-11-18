2fc91aa8ffb1bda33b24c4fb8dafc491784158b1
progfay <shun1856shun@gmail.com>
2020-11-18 23:33:27 +0900

    refactor(convertToTable): exclude padding length on calc

--- src/lib/convertToTable.ts
+++ src/lib/convertToTable.ts
@@ -2,25 +2,25 @@ const bold = (str: string): string => `\u001b[1m${str}\u001b[22m`
 const dim = (str: string): string => `\u001b[2m${str}\u001b[22m`
 
 export function convertToTable<T extends { [key: string]: any }> (objects: T[], orderedKeys: (keyof T)[]) {
-  const columnLength = orderedKeys.map(key => key.toString().length + 2)
+  const columnLength = orderedKeys.map(key => key.toString().length)
 
   for (let i = 0; i < orderedKeys.length; i++) {
     const key = orderedKeys[i] as string
     for (const object of objects) {
-      columnLength[i] = Math.max(columnLength[i], object[key].toString().length + 2)
+      columnLength[i] = Math.max(columnLength[i], object[key].toString().length)
     }
   }
 
-  const rowDelimiter = dim('+' + columnLength.reduce((accr, len) => `${accr}${'-'.repeat(len)}+`, ''))
+  const rowDelimiter = dim('+' + columnLength.reduce((accr, len) => `${accr} ${'-'.repeat(len)} +`, ''))
   const cellDelimiter = dim('|')
   let table = rowDelimiter + '\n' +
-    cellDelimiter + orderedKeys.map((key, i) => ` ${bold((key as string)[i === 0 ? 'padEnd' : 'padStart'](columnLength[i] - 2))} `).join(cellDelimiter) + cellDelimiter + '\n' +
+    cellDelimiter + orderedKeys.map((key, i) => ` ${bold((key as string)[i === 0 ? 'padEnd' : 'padStart'](columnLength[i]))} `).join(cellDelimiter) + cellDelimiter + '\n' +
     rowDelimiter + '\n'
 
   for (const object of objects) {
     table += cellDelimiter
     for (let i = 0; i < orderedKeys.length; i++) {
-      table += ` ${(object[orderedKeys[i]] as string)[i === 0 ? 'padEnd' : 'padStart'](columnLength[i] - 2)} ${cellDelimiter}`
+      table += ` ${(object[orderedKeys[i]] as string)[i === 0 ? 'padEnd' : 'padStart'](columnLength[i])} ${cellDelimiter}`
     }
     table += '\n'
     table += rowDelimiter + '\n'
