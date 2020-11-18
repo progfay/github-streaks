25cfe7af72d305386ee3cc520e84533626a33757
progfay <shun1856shun@gmail.com>
2020-11-19 00:24:07 +0900

    refactor(convertToTable): use Array.prototype.reduce for build table rows

--- src/lib/convertToTable.ts
+++ src/lib/convertToTable.ts
@@ -1,26 +1,23 @@
 const bold = (str: string): string => `\u001b[1m${str}\u001b[22m`
 const dim = (str: string): string => `\u001b[2m${str}\u001b[22m`
 
-export function convertToTable<T extends { [key: string]: any }> (objects: T[], orderedKeys: (keyof T)[]) {
+const CELL_DELIMITER = dim('|')
+
+function buildCellsRow (arr: string[], decorate: (str: string, index: number) => string = str => str) {
+  return CELL_DELIMITER + arr.map((elem, i) => ` ${decorate(elem, i)} `).join(CELL_DELIMITER) + CELL_DELIMITER
+}
+
+export function convertToTable<T extends { [key: string]: any }> (objects: T[], keys: (keyof T)[]) {
   const columnLength = objects.reduce(
-    (lens, obj) => orderedKeys.map((key, i) => Math.max(lens[i], obj[key].toString().length)),
-    orderedKeys.map(key => key.toString().length)
+    (lens, obj) => keys.map((key, i) => Math.max(lens[i], obj[key].toString().length)),
+    keys.map(key => key.toString().length)
   )
 
-  const rowDelimiter = dim('+' + columnLength.reduce((accr, len) => `${accr} ${'-'.repeat(len)} +`, ''))
-  const cellDelimiter = dim('|')
-  let table = rowDelimiter + '\n' +
-    cellDelimiter + orderedKeys.map((key, i) => ` ${bold((key as string)[i === 0 ? 'padEnd' : 'padStart'](columnLength[i]))} `).join(cellDelimiter) + cellDelimiter + '\n' +
-    rowDelimiter + '\n'
-
-  for (const object of objects) {
-    table += cellDelimiter
-    for (let i = 0; i < orderedKeys.length; i++) {
-      table += ` ${(object[orderedKeys[i]] as string)[i === 0 ? 'padEnd' : 'padStart'](columnLength[i])} ${cellDelimiter}`
-    }
-    table += '\n'
-    table += rowDelimiter + '\n'
-  }
-
-  return table
+  const rowDelimiter = dim('+' + columnLength.reduce((delimiter, len) => `${delimiter} ${'-'.repeat(len)} +`, ''))
+  return rowDelimiter + '\n' + [
+    buildCellsRow(keys.map(key => key.toString()), (str, i) => bold(str[i === 0 ? 'padEnd' : 'padStart'](columnLength[i]))),
+    ...objects.map(obj =>
+      buildCellsRow(keys.map(key => obj[key].toString()), (str, i) => str[i === 0 ? 'padEnd' : 'padStart'](columnLength[i]))
+    )
+  ].join(`\n${rowDelimiter}\n`) + '\n' + rowDelimiter
 }
